704aebced4b156437d318bccd612afdcca22fd36
MachDomo <dominic.c.ma@gmail.com>
2017-08-30 23:37:11 -0700

    "add for N rooks solution"

--- src/solvers.js
+++ src/solvers.js
@@ -18,8 +18,6 @@
 window.findNRooksSolution = function(n) {
   let solution = new Board({'n': n}); //fixme
   // hasAnyRooksConflicts()
-
-  let counter = 0;
   let rookRecursion = function(row, col) {
     // togglePiece(current row and column)
 
@@ -59,24 +57,150 @@ window.findNRooksSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+  let solution = new Board({'n': n});
 
+  let rookRecursion = function(row) {
+    // togglePiece(current row and column)
+
+    // Base Case - We stop when we find a solution for row = N
+
+    // What happens when we don't find a solution
+
+    //solution.attributes[row] = Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+
+      solution.togglePiece(row, i);
+      if (solution.hasAnyRooksConflicts()) {
+      // if conflict, togglePiece(current row and column)
+        solution.togglePiece(row, i);
+
+      } else {
+        if (row + 1 < n) {
+          rookRecursion(row + 1, 0);
+          solution.togglePiece(row, i);
+        } else if (row === n - 1) {
+          //console.log(solution.attributes);
+          solutionCount++;
+          solution.togglePiece(row, i);
+
+
+
+
+        }
+      }
+    }
+    // if no conflicts
+    // call rookRecursion on next row
+    // if we are about to go over n - 1 on rows, then don't do next line
+
+    // iterate through column
+    // return array of arrays (solution.attributes)
+  };
+  rookRecursion(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
-  // hasAnyQueenConflictsOn(rowIndex, colIndex)
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  // hasAnyQueenConflictsOn(rowIndex, colIndex)
+  let solution = new Board({'n': n}); //fixme
+  let array = [];
+  // hasAnyRooksConflicts()
+  let rookRecursion = function(row, col) {
+    // togglePiece(current row and column)
+
+    // Base Case - We stop when we find a solution for row = N
+    // What happens when we don't find a solution
+
+    debugger;
+    for (let i = 0; i < n; i++) {
+
+      solution.togglePiece(row, i);
+      if (solution.hasAnyQueensConflicts()) {
+      // if conflict, togglePiece(current row and column)
+        solution.togglePiece(row, i);
+
+      } else {
+        if (row + 1 < n) {
+          rookRecursion(row + 1, 0);
+          solution.togglePiece(row, i);
+        } else if (row === n - 1) {
+          //console.log(solution.attributes);
+          for (let i = 0; i < n; i++) {
+            array.push(solution.attributes[i]);
+          }
+          break;
+
+
+
+
+
+        }
+      }
+    }
+
+    // if no conflicts
+    // call rookRecursion on next row
+    // if we are about to go over n - 1 on rows, then don't do next line
+
+    // iterate through column
+    // return array of arrays (solution.attributes)
+  };
+  rookRecursion(0, 0);
+
+
+
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(array));
+  return array;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var solutionCount = 0; //fixme
+
+  let solution = new Board({'n': n});
+
+  let rookRecursion = function(row) {
+    // togglePiece(current row and column)
+
+    // Base Case - We stop when we find a solution for row = N
+
+    // What happens when we don't find a solution
+
+    //solution.attributes[row] = Array(n).fill(0);
+    for (let i = 0; i < n; i++) {
+
+      solution.togglePiece(row, i);
+      if (solution.hasAnyQueensConflicts()) {
+      // if conflict, togglePiece(current row and column)
+        solution.togglePiece(row, i);
+
+      } else {
+        if (row + 1 < n) {
+          rookRecursion(row + 1, 0);
+          solution.togglePiece(row, i);
+        } else if (row === n - 1) {
+          //console.log(solution.attributes);
+          solutionCount++;
+          solution.togglePiece(row, i);
+
+
+
+
+        }
+      }
+    }
+    // if no conflicts
+    // call rookRecursion on next row
+    // if we are about to go over n - 1 on rows, then don't do next line
+
+    // iterate through column
+    // return array of arrays (solution.attributes)
+  };
+  rookRecursion(0);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
